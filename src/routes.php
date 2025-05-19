f0e73de1dcd47f1278a03e123d0817f57267d1be
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-05-19 16:07:58 -0500

    first commit

---
+++ src/routes.php
@@ -0,0 +1,9 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
